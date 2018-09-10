24d966f4264f54cb53728bbb4a30d12262ebe074
Zach Ploskey <zach@ploskey.com>
2018-09-09 21:31:21 -0700

    Add snap and snap-diff package commands

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,7 @@
 module.exports = {
   testEnvironment: "node",
-  testPathIgnorePatterns: ['/node_modules/', '<rootDir>/__tests__/fixtures'],
+  testPathIgnorePatterns: [
+    '/node_modules/',
+    '<rootDir>/__tests__/fixtures',
+    '<rootDir>/snap'],
 };
